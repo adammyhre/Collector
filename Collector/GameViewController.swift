4a6c1450f26118c059a683d658b2cfa014952b8b
Adam Myhre <adam@undergalaxie.com>
2018-05-09 13:58:11 -0700

    ImageView ready

---
+++ Collector/GameViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GameViewController.swift
+//  Collector
+//
+//  Created by Bradley Myhre on 2018-05-09.
+//  Copyright © 2018 undergalaxie.com. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var titleTextfield: UITextField!
+    @IBOutlet weak var gameImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+}
