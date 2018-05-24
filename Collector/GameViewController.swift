93fe3415d97d1715104f27c67926f22a57f20406
Adam Myhre <adam@undergalaxie.com>
2018-05-24 08:30:18 -0700

    Item listing complete (needs some tuning, but is functional)

--- Collector/GameViewController.swift
+++ Collector/GameViewController.swift
@@ -8,21 +8,45 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var titleTextfield: UITextField!
     @IBOutlet weak var gameImageView: UIImageView!
     
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
     }
 
     @IBAction func addTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let game = Game(context: context)
+        game.title = titleTextfield.text
+        game.image = UIImagePNGRepresentation(gameImageView.image!) as NSData?
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        navigationController!.popViewController(animated: true)
     }
     
     @IBAction func cameraTapped(_ sender: Any) {
     }
     
     @IBAction func photosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        gameImageView.image = image
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 }
