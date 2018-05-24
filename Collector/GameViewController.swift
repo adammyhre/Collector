210f25806278c625ab156d07e0234eb83600d318
Adam Myhre <adam@undergalaxie.com>
2018-05-24 09:21:31 -0700

    Added camera functionality

--- Collector/GameViewController.swift
+++ Collector/GameViewController.swift
@@ -52,6 +52,10 @@ class GameViewController: UIViewController, UIImagePickerControllerDelegate, UIN
     }
     
     @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        
+        present(imagePicker, animated: true, completion: nil)
+        
     }
     
     @IBAction func photosTapped(_ sender: Any) {
